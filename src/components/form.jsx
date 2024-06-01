f10391b3232d041aa02e3431fb148d76db8101b7
garbhagudi <garbhagudi.11@gmail.com>
2024-06-01 08:55:59 +0530

    Update iframe source and dependencies

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,7 +2,7 @@ const IndexPage = () => {
   return (
     <>
       <iframe
-        src='/contact.html'
+        src='/contact2.html'
         className='w-screen max-w-xs bg-transparent mx-auto h-screen max-h-[460px]'
         loading='lazy'
       >
