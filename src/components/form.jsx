0fa673b7164c95e72fac8c41d577b40fca7c2aa5
garbhagudi <garbhagudi.11@gmail.com>
2024-06-01 09:04:36 +0530

    Update iframe source to fix contact form display issue

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,7 +2,7 @@ const IndexPage = () => {
   return (
     <>
       <iframe
-        src='/contact2.html'
+        src='/contact.html'
         className='w-screen max-w-xs bg-transparent mx-auto h-screen max-h-[460px]'
         loading='lazy'
       >
