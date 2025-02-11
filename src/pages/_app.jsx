f678306dd20e624beec610ae2fdc379a1a7d4a6d
Tamil Arasan <tamil@witarist.com>
2025-02-11 10:31:17 +0530

    SalesIQ removed in the ivf/treatment cost LP

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { ThemeProvider, Flex } from 'theme-ui';
+import { useRouter } from 'next/router';
 import theme from 'theme';
 import '../styles/globals.css';
 import '../styles/calendar.css';
@@ -15,12 +16,23 @@ const Footer = dynamic(() => import('components/footer/footer'), {
 });
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [showSalesIQ, setShowSalesIQ] = useState(true);
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const TagManager = require('react-gtm-module');
       TagManager.initialize({ gtmId: 'GTM-NT9BZ69' });
     }
   }, []);
+
+  useEffect(() => {
+    if (router.pathname === '/ivf/treatment-cost') {
+      setShowSalesIQ(false);
+    } else {
+      setShowSalesIQ(true);
+    }
+  }, [router.pathname]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -34,9 +46,9 @@ function MyApp({ Component, pageProps }) {
         <main sx={{ variant: 'layout.main' }}>
           <Component {...pageProps} />
         </main>
-        <FloatWhatsApp />
+        <FloatWhatsApp condition={showSalesIQ} />
         <FloatPhone />
-        <SalesIQ />
+        {showSalesIQ && <SalesIQ />}
         <Footer />
       </Flex>
     </ThemeProvider>
