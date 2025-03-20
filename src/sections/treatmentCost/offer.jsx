f048777a197fa10ed1a367e67be1d4d1556b5575
ashish-witarist <ashish@witarist.com>
2025-03-20 14:55:54 +0530

    updated the treatement page

--- src/sections/treatmentCost/offer.jsx
+++ src/sections/treatmentCost/offer.jsx
@@ -4,24 +4,59 @@ import FormComponent from 'components/formComp';
 
 const Offer = () => {
   return (
-    <section className='flex flex-wrap'>
-      <div className='h-100 mx-auto w-full bg-cover bg-center object-cover md:w-1/2'>
+    <section className='grid grid-cols-1 lg:grid-cols-3 md:pb-8 md:px-5 pb-5 px-2.5'>
+      <div className='order-1 md:order-0'>
+        <div className='flex flex-col bg-cover bg-no-repeat h-full justify-center text-center items-left lg:py-0 py-5'>
+          <div className='text-2xl text-center font-extrabold font-heading pb-4 pt-8 px-2'>
+            <h1 className='text-[#e92e64] uppercase'>
+              IVF Treatment Cost at ₹1,50,000*
+            </h1>
+          </div>
+          <div className='flex flex-col text-xl font-content font-normal pb-4 px-4 py-2'>
+            <div className='text-start'>This base package includes :</div>
+            <ul className='flex flex-col text-start items-start mt-2 space-y-1'>
+              <li>1. Ovum Pick-up (OPU)</li>
+              <li>2. Embryo Freezing for 1 Year (Up to 2 straws)</li>
+              <li>3. Frozen Embryo Transfer (FET)</li>
+            </ul>
+            <div className='flex justify-end pt-16'>
+              <Image
+                src='https://res.cloudinary.com/garbhagudiivf/image/upload/v1742457439/1_Lakh_Offer_mbs1hf.png'
+                width={800}
+                height={350}
+                alt='Start IVF from as low as 90K'
+                quality={90}
+                priority
+                // className='h-60 w-60'
+                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+              />
+            </div>
+          </div>
+        </div>
+        <div className='text-xs font-content font-medium hidden lg:block mt-3'>
+          *terms and conditions apply
+        </div>
+      </div>
+
+      <div className='flex order-0 md:order-1 overflow-hidden'>
         <Image
-          src='https://res.cloudinary.com/garbhagudiivf/image/upload/v1738754618/IVF-Starts-at-150K_1to1_wyv3fv.png'
-          alt='Banner Image'
-          width={400}
-          height={400}
-          className='mx-auto h-full w-full object-contain'
+          src='https://res.cloudinary.com/garbhagudiivf/image/upload/v1742454450/Couple--01_is1s40.png'
+          width={800}
+          height={350}
+          alt='Start IVF from as low as 90K'
+          quality={90}
           priority
+          className='h-full'
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
         />
       </div>
-      <div className='flex w-full flex-col items-center justify-center p-4 md:w-1/2'>
-        <h2 className='font-lexend text-center text-3xl font-bold text-gg-500 lg:text-5xl'>
+      <div className='flex flex-col order-2 justify-center p-4 w-full items-center'>
+        <h2 className='text-center text-gg-500 text-xl font-bold font-lexend lg:text-3xl'>
           Bengaluru&apos;s Most Trusted Fertility Centre
         </h2>
 
-        <div className='my-6 rounded-lg bg-[#005E7E] pt-10 lg:px-6 lg:pt-4'>
-          <h2 className='font-lexend overflow-hidden rounded-lg text-center text-xl font-bold text-white'>
+        <div className='bg-[#005E7E] rounded-lg lg:pt-4 lg:px-6 my-6 pt-10'>
+          <h2 className='rounded-lg text-center text-white text-xl font-bold font-lexend overflow-hidden'>
             Talk To Our Fertility Experts Today
           </h2>
           <FormComponent />
--- src/sections/treatmentCost/treatment.jsx
+++ src/sections/treatmentCost/treatment.jsx
@@ -1,76 +1,197 @@
+import React, { useState } from 'react';
 import Image from 'next/image';
-import React from 'react';
 
-const treatment = () => {
+const Treatment = () => {
+  const [showMore, setShowMore] = useState(false);
+
+  const handleToggle = () => {
+    setShowMore((prevShowMore) => !prevShowMore);
+  };
+
   return (
     <div className='scroll-m-16 scroll-smooth' id='know-more'>
-      <div className='relative bg-white dark:bg-gray-800'>
-        <div className='container relative m-auto px-3 md:px-12 lg:px-6'>
-          <h3 className='mx-auto text-left font-heading text-4xl font-black lg:text-5xl'>
-            What is IVF and how does it work ?
+      <div className='bg-white dark:bg-gray-800 py-5 relative'>
+        <div className='container m-auto lg:px-6 md:px-12 px-3 relative'>
+          <h3 className='text-4xl text-left font-black font-heading lg:text-5xl mx-auto'>
+            Step by Step IVF Process
           </h3>{' '}
-          <div className='mx-auto flex-wrap text-gray-800 md:flex dark:text-gray-200'>
-            <div className='grid grid-cols-1 items-center lg:grid-cols-2'>
-              <div className='font-content relative mx-auto ml-0 mt-8 space-y-8 text-left md:mt-16 md:max-w-lg lg:max-w-2xl'>
-                <p className=''>
-                  GarbhaGudi IVF Center offers state-of-the-art facilities and a
-                  team of experienced doctors and embryologists who work
-                  together to provide personalized care and support to couples
-                  undergoing IVF in Bangalore. With the help of IVF, couples
-                  struggling with infertility can have a chance to conceive and
-                  start a family.
-                  <br />
-                  <br />
-                  To conceive. IVF or In Vitro Fertilization is one of the more
-                  widely known types of Assisted Reproductive Techniques (ART).
-                  It involves retrieving eggs from the woman&apos;s ovaries and
-                  fertilizing them with sperm in a laboratory dish. The
-                  resulting embryos are then transferred back into the
-                  woman&apos;s uterus in the hope of achieving a successful
-                  pregnancy. GarbhaGudi IVF Center offers state-of-the-art
-                  facilities and a team of experienced doctors and embryologists
-                  who work together to provide personalized care and support to
-                  couples undergoing IVF in Bangalore. With the help of IVF,
-                  couples struggling with infertility can have a chance to
-                  conceive and start a family.
-                </p>
+          <div className='flex-wrap text-gray-800 dark:text-gray-200 md:flex mx-auto'>
+            <div className='grid grid-cols-1 lg:grid-cols-5'>
+              <div className='col-span-3 order-2 font-content lg:order-1 mt-8 relative'>
                 <p>
-                  IVF treatment in Bangalore is a multi-step process that
-                  involves several stages, including ovarian stimulation, egg
-                  retrieval, fertilization, embryo culture, and embryo transfer.
-                  Our team of experienced fertility specialists carefully
-                  monitors the process, and work closely with patients to
-                  customize a treatment plan that meets their unique needs and
-                  maximizes their chances of success.
+                  GarbhaGudi IVF Center in Bangalore combines advanced
+                  technology with a skilled team of doctors and embryologists to
+                  offer tailored IVF treatments, helping couples overcome
+                  infertility and achieve their dream of parenthood. Embarking
+                  on your first in vitro fertilization (IVF) cycle can be both
+                  exciting and overwhelming. Understanding each step of the
+                  process can help ease your anxieties and prepare you for the
+                  journey ahead. We’ll break down the entire IVF process,
+                  providing a detailed overview of what to expect during each
+                  stage for clear understanding.
                 </p>
-                <p>
-                  One full IVF cycle takes about three weeks but sometimes the
-                  procedure is split into parts, to manage certain challenges in
-                  the couples. This may delay the process. IVF can be done using
-                  the woman&apos;s eggs and the male partner&apos;s sperm or it
-                  may involve sperm, eggs, or embryos from anonymous donors.
-                </p>
-                <p>
-                  It is important to note that not all IVF cycles result in
-                  pregnancy, and it may be necessary to try multiple cycles
-                  before achieving a successful pregnancy. The success rate of
-                  IVF depends on a variety of factors, including the
-                  person&apos;s age, the cause of infertility, and the quality
-                  of the eggs and sperm.
-                </p>
-              </div>
-              <div className='-right-0 mx-auto mt-4 max-w-[50rem] lg:p-4'>
-                <div className='relative w-full'>
-                  <Image
-                    src='https://res.cloudinary.com/garbhagudiivf/image/upload/v1722510346/Misc/PNG_25_yoa08y_1_cr2fpg.png'
-                    className='relative w-full bg-white/10 dark:brightness-75 dark:invert'
-                    alt='Process of IVF Treatment in Bangalore'
-                    loading='lazy'
-                    width={500}
-                    height={500}
-                  />
-                  ;
+
+                {/* See More/See Less Toggle Section */}
+                <ul className='flex flex-col list-decimal gap-1.5 mt-2 px-2.5'>
+                  {/* Show top 3 list items by default */}
+                  <li className='text-lg font-bold'>
+                    Initial Consultation and Testing
+                    <br />
+                    <div className='text-base font-semibold'>
+                      The Initial Consultation
+                    </div>
+                    <div className='text-sm font-normal'>
+                      The IVF journey begins with a comprehensive consultation
+                      with a fertility specialist. During this appointment, the
+                      doctor will:
+                    </div>
+                    <ul className='list-disc text-sm font-normal px-5'>
+                      <li>
+                        Review your medical history and any previous fertility
+                        treatments.
+                      </li>
+                      <li>
+                        Perform a physical examination and ultrasound to
+                        evaluate reproductive health.
+                      </li>
+                      <li>
+                        Discuss potential causes of infertility and determine if
+                        IVF is the right option for you.
+                      </li>
+                    </ul>
+                  </li>
+
+                  <li className='text-lg font-bold'>
+                    Preparing for Your IVF Cycle
+                    <br />
+                    <div className='text-base font-semibold'>
+                      Birth Control or Hormonal Regulation
+                    </div>
+                    <div className='text-sm font-normal'>
+                      Many IVF cycles start with birth control pills or hormonal
+                      medications to regulate your menstrual cycle. This step
+                      ensures your ovaries respond predictably to stimulation
+                      medications.
+                    </div>
+                  </li>
+
+                  <li className='text-lg font-bold'>
+                    Ovarian Stimulation
+                    <br />
+                    <div className='text-base font-semibold'>
+                      Ovarian Stimulation
+                    </div>
+                    <div className='text-sm font-normal'>
+                      The next phase involves stimulating the ovaries to produce
+                      multiple eggs. This is achieved through daily injections
+                      of fertility medications such as:
+                    </div>
+                    <ul className='list-disc text-sm font-normal px-5'>
+                      <li>
+                        <span className='text-base font-semibold'>
+                          Gonadotropins:
+                        </span>{' '}
+                        To stimulate egg production.
+                      </li>
+                      <li>
+                        <span className='text-base font-semibold'>
+                          GnRH Agonists or Antagonists:
+                        </span>{' '}
+                        To prevent premature ovulation.
+                      </li>
+                    </ul>
+                  </li>
+
+                  {/* Conditional rendering for the remaining list items */}
+                  {showMore && (
+                    <>
+                      <li className='text-lg font-bold'>
+                        Egg Retrieval
+                        <br />
+                        <div className='text-base font-semibold'>
+                          The Procedure
+                        </div>
+                        <div className='text-sm font-normal'>
+                          Egg retrieval is a minimally invasive procedure
+                          performed under sedation. Using an ultrasound-guided
+                          needle, the doctor retrieves eggs from the follicles
+                          in the ovaries.
+                        </div>
+                      </li>
+
+                      <li className='text-lg font-bold'>
+                        Fertilization and Embryo Development
+                        <br />
+                        <div className='text-base font-semibold'>
+                          Fertilization Process
+                        </div>
+                        <div className='text-sm font-normal'>
+                          Once the eggs are retrieved, they are combined with
+                          sperm in the laboratory. Two main methods of
+                          fertilization are used:
+                        </div>
+                      </li>
+
+                      <li className='text-lg font-bold'>
+                        Embryo Transfer
+                        <br />
+                        <div className='text-base font-semibold'>
+                          Timing of Transfer
+                        </div>
+                        <div className='text-sm font-normal'>
+                          Embryo transfer typically occurs 3-5 days after egg
+                          retrieval.
+                        </div>
+                      </li>
+
+                      <li className='text-lg font-bold'>
+                        The Two-Week Wait
+                        <br />
+                        <div className='text-base font-semibold'>
+                          What Happens During This Period?
+                        </div>
+                        <div className='text-sm font-normal'>
+                          After the embryo transfer, you’ll enter the two-week
+                          wait before taking a pregnancy test.
+                        </div>
+                      </li>
+
+                      <li className='text-lg font-bold'>
+                        Pregnancy Test and Next Steps
+                        <br />
+                        <div className='text-base font-semibold'>
+                          Taking the Test
+                        </div>
+                        <div className='text-sm font-normal'>
+                          Approximately two weeks after the embryo transfer, a
+                          blood test will measure HCG levels to confirm
+                          pregnancy.
+                        </div>
+                      </li>
+                    </>
+                  )}
+                </ul>
+
+                {/* See More/See Less Button */}
+                <div className='mt-4'>
+                  <button
+                    onClick={handleToggle}
+                    className='text-base text-primary-600 dark:text-primary-400 font-semibold hover:underline'
+                  >
+                    {showMore ? 'See Less' : 'See More'}
+                  </button>
                 </div>
+              </div>
+
+              <div className='col-span-2 order-1 lg:order-2 lg:p-4 max-w-[50rem] md:max-w-full mt-4 mx-auto'>
+                <Image
+                  src='https://res.cloudinary.com/garbhagudiivf/image/upload/v1722510346/Misc/PNG_25_yoa08y_1_cr2fpg.png'
+                  className='bg-white/10 w-full dark:brightness-75 dark:invert relative'
+                  alt='Process of IVF Treatment in Bangalore'
+                  loading='lazy'
+                  width={500}
+                  height={500}
+                />
               </div>
             </div>
           </div>
@@ -80,4 +201,4 @@ const treatment = () => {
   );
 };
 
-export default treatment;
+export default Treatment;
