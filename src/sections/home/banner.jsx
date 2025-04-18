24e69ad3ecaa45e386a1a9dbe945587a9b4860c3
ashish-witarist <ashish@witarist.com>
2025-04-18 19:00:18 +0530

    changed banner and add h1 tag

--- src/sections/home/banner.jsx
+++ src/sections/home/banner.jsx
@@ -26,51 +26,33 @@ const Banner = () => {
         />
       </Head>
 
-      <div className='grid grid-cols-1 pb-5 md:pb-8 lg:grid-cols-3'>
-        <div className='md:order-0 order-1'>
-          <div className='items-left flex h-full flex-col justify-center bg-cover bg-no-repeat py-16 text-center lg:py-0'>
-            <div className='px-2 pb-4 text-center text-2xl font-extrabold'>
-              <h1 className='uppercase text-[#e92e64]'>
-                Leading IVF & Fertility Clinic in Bangalore: Expert Care &
-                Advanced Treatments
-              </h1>
-            </div>
-            <div className='flex flex-col items-center px-4 py-2 pb-4 text-center text-xl font-normal'>
-              <div>
-                Offer of the Month: <br />
-                15th Anniversary Special: <br />
-                <strong className='text-brandPurpleDark'>
-                  ₹1,00,000 off on IVF Treatment
-                </strong>
-              </div>
-              <p className='mt-2'>Offer Validity: 30th April 2025</p>
-            </div>
-            <Popup
-              title={'Book Free Consultation'}
-              btnClassName='rounded-md bg-brandPink px-6 py-4 font-semibold text-white focus:outline-none'
-            />
-          </div>
-          <div className='mt-3 hidden text-xs font-medium lg:block'>
-            *terms and conditions apply
-          </div>
-        </div>
-
-        <div className='order-0 flex items-end justify-center overflow-hidden md:order-1'>
+      <div className='relative grid grid-cols-1 gap-y-3 pb-5 md:pb-8 lg:grid-cols-3'>
+        <div className='relative col-span-2'>
           <Image
-            src='https://res.cloudinary.com/garbhagudiivf/image/upload/v1741775546/15th_year_anniversary_Landing_page-01_g71qgd.webp'
+            src='https://res.cloudinary.com/garbhagudiivf/image/upload/v1744978945/April_Month_Landing_Page_Latest-02_zle4xm.webp'
             width={490}
             height={360}
             alt='Start IVF from as low as 90K'
             quality={90}
             priority='true'
             sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+            className='h-full w-full'
           />
+          <div className='absolute -bottom-6 flex w-full justify-center p-2 font-semibold text-white md:hidden'>
+            <h1 className='w-full rounded-md bg-gg-500 p-1 text-center text-sm shadow-sm'>
+              Best IVF & Fertility Clinic - Affordable IVF Treatment
+            </h1>
+          </div>
         </div>
-
-        <div className='order-2 bg-[#005e7e]' id='leadForm'>
-          <div className='flex items-center justify-center'>
+        <div className='flex justify-center bg-[#005e7e]' id='leadForm'>
+          <div className='flex h-fit w-full items-center justify-center'>
             <FormComponent title='Book Your Appointment' isTag={false} />
           </div>
+        </div>
+        <div className='absolute -bottom-5 hidden w-full justify-center px-5 font-semibold text-white md:flex'>
+          <h1 className='w-full rounded-md bg-gg-500 p-2 text-center text-base shadow-sm'>
+            Best IVF & Fertility Clinic - Affordable IVF Treatment
+          </h1>
         </div>
       </div>
     </div>
