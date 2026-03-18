eab5df769451fef153b9b9fe57d5255b6e284f7a
sdeveloper123 <sunil@witarist.com>
2026-03-18 12:51:45 +0530

    Added script

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -105,6 +105,22 @@ function MyApp({ Component, pageProps }) {
         `,
           }}
         />
+        <Script
+          id='hotjar'
+          strategy='lazyOnload'
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:6671155,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
         <main sx={{ variant: 'layout.main' }}>
           <Component {...pageProps} />
         </main>
