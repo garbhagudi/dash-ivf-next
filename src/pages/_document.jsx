6a7f9d9900413cfd153173db1b5bbb16e2702649
sdeveloper123 <sunil@witarist.com>
2026-03-27 16:10:53 +0530

    Fixed issues for GTM

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,20 +3,18 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      {/* <Head> */}
-      {/* Google Tag Manager */}
-      {/* <script
+      <Head>
+        {/* Google Tag Manager — high in <head> per Google */}
+        <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NT9BZ69');`,
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NSZH8N9M');`,
           }}
-        /> */}
-      {/* End Google Tag Manager */}
-      {/* </Head> */}
-      <Head>
+        />
+        {/* End Google Tag Manager */}
         <link
           rel='preload'
           href='/fonts/DM-Sans.woff2'
@@ -31,7 +29,7 @@ export default function Document() {
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src='https://www.googletagmanager.com/ns.html?id=GTM-NT9BZ69'
+            src='https://www.googletagmanager.com/ns.html?id=GTM-NSZH8N9M'
             height='0'
             width='0'
             style={{ display: 'none', visibility: 'hidden' }}
